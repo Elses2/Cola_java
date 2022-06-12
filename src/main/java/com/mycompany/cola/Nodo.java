4208404c94f725c5a55bb808ea40c866eb82c046
eber <eberchiecher2@gmail.com>
2022-06-12 12:21:58 -0300

    Creacion de clase Nodo(terminada)y creacion de clase cola(sin terminar)

---
+++ src/main/java/com/mycompany/cola/Nodo.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cola;
+
+/**
+ *
+ * @author elses
+ */
+public class Nodo {
+    
+    private Object info;///Los datos que va a almacenar el nodo
+ //al ser tipo object puede almacenar cualquier tipo de objeto
+    
+    private Nodo siguiente;///Creamos una variaable del mismo tipo de la clase que la contiene
+
+    public Nodo(Object info, Nodo siguiente){
+        setInfo(info);
+        setSiguiente(siguiente);
+    }
+    public Nodo(Object info){
+        setInfo(info);
+        setSiguiente(null);
+    }
+    public Nodo(){
+    setInfo(null);
+    setSiguiente(null);
+    }
+
+    public void setInfo(Object info) {
+        this.info = info;
+    }
+
+ 
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+}
